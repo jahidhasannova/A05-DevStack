7fa446e05ef99d1ac77841734c549f1d4381806f
User <user@Users-MacBook-Air.local>
2026-09-12 02:37:06 +0600

    initialize React project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hello Dev</h2>
+
+
+    </>
+  )
+}
+
+export default App
